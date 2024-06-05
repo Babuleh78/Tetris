6257703eacccc1221a2016f742aad30cae9c1c2e
Babuleh78 <150275581+Babuleh78@users.noreply.github.com>
2024-06-05 23:01:11 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,151 @@
+#include "raylib.h"
+#include <iostream>
+#include "rlgl.h"
+#include <deque>
+#include <raymath.h>
+
+int cellSize = 30;
+int cellCount = 25;
+double lastUpdTime = 0;
+using namespace std;
+bool updTriggered(double inter) {
+	double currentTime = GetTime();
+	if (currentTime - lastUpdTime >= inter) {
+		lastUpdTime = currentTime;
+		return true;
+	}
+	return false;
+}
+void Equal_matrix(Vector2* matrix, Vector2* _matrix) {
+	for (int i = 0; i < 4; i++) {
+		matrix[i] = _matrix[i];
+	}
+}
+class Tetramino {
+
+
+public:
+	void Update() {
+		int a = max(matrix[0].y, matrix[1].y), b= (matrix[2].y, matrix[3].y);
+		a = max(a, b);
+		if (a != 19) {
+			for (int i = 0; i < 4; i++) {
+				matrix[i] = Vector2Add(matrix[i], y1);
+			}
+			y_perem += 1;
+		}
+	}
+	void Draw() {
+		for (int i = 0; i < 4; i++) {
+			int x = matrix[i].x; int y = matrix[i].y;
+
+			DrawRectangle(x * cellSize, y * cellSize, cellSize, cellSize, color);
+		}
+
+	}
+	void Move(bool flag) { //flag == true вправо иначе влево
+		
+		for (int i = 0; i < 4; i++) {
+			matrix[i] = (flag ? (Vector2Add(matrix[i], x1)) : Vector2Add(matrix[i], Vector2Scale(x1, -1)));
+			
+		}
+		if (flag) { x_perem += 1; }
+		else { x_perem -= 1; }
+	}
+	Vector2 y1 = { 0,1 };
+	Vector2 e = { 1, 1 };
+	Vector2 x1 = { 1, 0 };
+	int x_perem = 10;
+	int y_perem = 0;
+	Color color;	
+	Vector2 matrix[4];
+	Vector2 matrix0[4];
+	Vector2 matrix1[4];
+	Vector2 matrix2[4];
+	Vector2 matrix3[4];
+	
+	int condition;
+};
+class TBlock : public Tetramino {
+public:
+
+	TBlock(){
+		matrix[0] = { 1+10,1 };matrix[1] = {1+10,0 };matrix[2] = { 2+10, 0};matrix[3] = { 0+10, 0};//0-точка вращения //		
+		matrix0[0] = { 1,1 }; matrix0[1] = { 0, 0 }; matrix0[2] = { 1, 0 }; matrix0[0] = { 2, 0 };
+		matrix1[0] = { 1, 1 }; matrix1[1] = { 2, 0 }; matrix1[2] = { 2, 1 }; matrix1[3] = { 2,2 };
+		matrix2[0] = { 1, 1 }; matrix2[1] = { 2, 2 }; matrix2[2] = { 1,2 }; matrix2[3] = { 0, 2 };
+		matrix3[0] = { 1,1 }; matrix3[1] = { 0,2 }; matrix3[2] = { 0,1 }; matrix3[3] = { 0,0 };
+		condition = 0;
+		color = GREEN;
+	}
+	
+	void Rotate(bool flag) {//flag == true по часовой
+		if (flag) {
+			condition = (condition + 1 < 4 ? condition + 1 : 0);
+		}
+		else {
+			condition = (condition - 1 >= 0 ? condition - 1 : 3);
+		}
+		switch (condition) {
+		case 0: Equal_matrix(matrix, matrix0);  break;
+		case 1: Equal_matrix(matrix, matrix1); break;
+		case 2: Equal_matrix(matrix, matrix2); break;
+		case 3: Equal_matrix(matrix, matrix3); break;
+		}
+		for (int i = 0; i < 4; i++) {
+			matrix[i] = Vector2Add(matrix[i], Vector2Scale(x1, x_perem));
+			matrix[i] = Vector2Add(matrix[i], Vector2Scale(y1, y_perem));
+		}
+		
+	}
+
+};
+
+class QuadroBlock : public Tetramino {
+public:
+	QuadroBlock() {
+		matrix[0] = { 0 + 10,0 };matrix[1] = { 1 + 10,0 };matrix[2] = { 0 + 10, 1 };matrix[3] = { 1 + 10, 1 };//0-точка вращения //		
+		color = YELLOW;
+	}
+
+	void Rotate(bool flag) {//flag == true по часовой
+		return;
+
+	}
+};
+int main() {
+	SetTraceLogLevel(LOG_NONE);
+	InitWindow(800, 600, "Tetris");
+	SetTargetFPS(60);
+	QuadroBlock a;
+	//TBlock a;
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+		a.Draw();
+		
+		ClearBackground(BLUE);
+		if (updTriggered(0.2)) {
+			a.Update();
+			if (IsKeyDown(KEY_D)) {
+				a.Move(true);
+			}
+			else if (IsKeyDown(KEY_A)) {
+				a.Move(false);
+			}
+		}
+		if (IsKeyPressed(KEY_RIGHT)) {
+			a.Rotate(true);
+		}
+		else if (IsKeyPressed(KEY_LEFT)) {
+			a.Rotate(false);
+		}
+		/*else if (IsKeyDown(KEY_D)) {
+			a.Move(true);
+		}
+		else if (IsKeyDown(KEY_A)) {
+			a.Move(false);
+		}*/
+		EndDrawing();
+	}
+
+}
